002cc82f9cf4c08823ac1bcfd4ff02fd766f74d9
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2023-12-23 19:07:58 +0530

    compete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contect";
+function card(obj){
+    return (
+		<Card
+			name={obj.name}
+			imgURL={obj.imgURL}
+			phone={obj.phone}
+			email={obj.email}
+		/>
+	);
+}
 function App(){
+
      
     return (
         <div >
         <h1 className="heading">My Contacts</h1>
-        <Card />
-        <Card />
+       {contacts.map((obj)=>card(obj))}
         </div>
-         
     )
 }
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./card.css";
-function Card(){
-    return <div className="card">
-        <div className="head">
-            <h1>Beyonce</h1>  
-            <img src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" alt="" />   
-        </div>
-        
-    </div>
+function Card(props){
+    return (
+		<div className="card">
+			<div className="head">
+				<h1>{props.name}</h1>
+				<img src={props.imgURL} alt="avatar" />
+			</div>
+			<div className="foot">
+				<p>{props.phone}</p>
+				<p>{props.email}</p>
+			</div>
+		</div>
+	);
 }
 export default Card;
