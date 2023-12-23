fd26cf382fc1497992677b068269ca5f183d0028
sakthivel155 <sakthivelramesh155@gmail.com>
2023-12-23 17:39:58 +0530

    fgdg

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contect";
+function App(){
+     
+    return (
+        <div >
+        <h1 className="heading">My Contacts</h1>
+        <Card />
+        <Card />
+        </div>
+         
+    )
+}
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card.css";
+function Card(){
+    return <div className="card">
+        <div className="head">
+            <h1>Beyonce</h1>  
+            <img src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" alt="" />   
+        </div>
+        
+    </div>
+}
+export default Card;
